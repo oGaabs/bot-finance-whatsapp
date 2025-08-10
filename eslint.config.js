74aa8a04f1d12bd90811ee273b90f7531c8e9244
Gabriel Santana <gabriel04.ok@gmail.com>
2025-08-10 15:11:22 -0300

    add: update eslint config with padding line and non block statement

--- eslint.config.js
+++ eslint.config.js
@@ -11,7 +11,13 @@ export default defineConfig([
     rules: {
       semi: ["error", "never"],
       indent: ["error", 2, { SwitchCase: 1 }],
-      "no-tabs": "error"
+      "nonblock-statement-body-position": ["error", "below"],
+      "padding-line-between-statements": [
+        "error",
+        { blankLine: "always", prev: "*", next: "return" },
+        { blankLine: "always", prev: ["const", "let", "var"], next: "*" },
+        { blankLine: "any", prev: ["const", "let", "var"], next: ["const", "let", "var"] } // permite agrupamentos consecutivos
+      ]
     }
   },
 ])
